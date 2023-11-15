83ea6d5517b5f24f8eb6f67042a0249dd829e72e
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-11-15 00:59:14 -0300

    pages

--- src/Genre.js
+++ src/Genre.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect} from "react";
-import BannerGenre from "./images/banner-genres.jpg";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Card from "./components/Card";
@@ -35,10 +34,7 @@ const Genre = () => {
   return (
     <div>
       <Navbar />
-      <div className="banner-container">
-        <img src={BannerGenre} className="col-12 object-fit-cover" height={420} />
-        <p className="banner-title">Descubre nuevos mundos</p>
-      </div>
+
       <Title title={genreName} />
       <div className="flex-wrap d-flex col-11 m-auto">
         {books.map(book => {
